72b4290b90e2dc2ee7c6582d229ff5a7a7bb9346
eurosecom <eurosecom1@gmail.com>
2016-06-13 16:46:46 +0200

    Scrolling Colapsed AppBar
    #3
    - change Listview inventory to RecycleViewer at activity
    InventuraSDnewActivity.java

--- app/src/main/java/com/eusecom/saminveantory/InventuraSDnewActivity.java
+++ app/src/main/java/com/eusecom/saminveantory/InventuraSDnewActivity.java
@@ -1,20 +1,95 @@
 package com.eusecom.saminveantory;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InventuraSDnewActivity extends AppCompatActivity {
+
+    EditText inputMno, inputEan, inputAll;
+    Button btnObjednaj, btnScan, btnFakturuj;
+    TextView inputAllServer, popisEan;
+
+    private ProgressDialog pDialog;
+
+    private static final String TAG_PIDX = "pidx";
+    private static final String TAG_PID = "pid";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_PRICE = "price";
+    private static final String TAG_MNOX = "mnox";
+    private static final String TAG_CPL = "cpl";
+
+
+    String adresarxx="", cplx;
+    String ipx="0";
+
+    public ArrayList<HashMap<String, String>> productsList;
+    public Comparator<Map<String, String>> mapComparator;
+    RecyclerView recyclerView;
+    InventuraSDnewAdapter adapter;
+
+    private List<String> mDataSet = new ArrayList<String>();
+    private List<String> myAskList = new ArrayList<String>();
+    private List<String> myBidList = new ArrayList<String>();
+    private List<String> myProfitList = new ArrayList<String>();
+
+    private static String[] datas = null;
+    private static String[] asks = null;
+    private static String[] bids = null;
+    private static String[] profs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.invsd_activity_scrolling);
+
+        View bf = findViewById(R.id.btnFakturuj);
+        bf.setVisibility(View.GONE);
+        View bs  = findViewById(R.id.btnScan);
+        bs.setVisibility(View.INVISIBLE);
+
+        inputEan = (EditText) findViewById(R.id.inputEan);
+
+        inputAllServer = (TextView) findViewById(R.id.inputAllServer);
+        inputAllServer.setText(SettingsActivity.getServerName(this));
+
+        String serverx = inputAllServer.getText().toString();
+        String delims3 = "[/]+";
+        String[] serverxxx = serverx.split(delims3);
+        if (serverxxx.length < 2 ) {
+            adresarxx="androideshop";
+        }else{
+            adresarxx=serverxxx[1];
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -22,11 +97,26 @@ public class InventuraSDnewActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+
+                IntentIntegrator integrator = new IntentIntegrator(InventuraSDnewActivity.this);
+                integrator.initiateScan();
             }
         });
-    }
+
+        // Hashmap for ListView
+        productsList = new ArrayList<HashMap<String, String>>();
+
+        // Loading products in Background Thread
+        new LoadAllSDProducts().execute();
+
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+    }//end oncreate
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,4 +138,187 @@ public class InventuraSDnewActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-}
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            String re = scanResult.getContents();
+            inputEan.setText(re);
+        }
+
+        if (resultCode == 101) {
+
+            String pidxy = data.getStringExtra(TAG_PIDX);
+            //String pidxy = "0000000000001";
+
+            inputEan = (EditText) findViewById(R.id.inputEan);
+            inputEan.setText(pidxy);
+            inputMno.requestFocus();
+
+        }
+
+    }//end onactivityresult
+
+
+    class LoadAllSDProducts extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(InventuraSDnewActivity.this);
+            pDialog.setMessage(getString(R.string.progallproducts));
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+
+            // write on SD card file data in the text box
+            try {
+
+                String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+                String fileName = "/eusecom/" + adresarxx + "/inventura.csv";
+                File myFile = new File(baseDir + File.separator + fileName);
+
+                FileInputStream fIn = new FileInputStream(myFile);
+                BufferedReader myReader = new BufferedReader(
+                        new InputStreamReader(fIn));
+                String aDataRow = "";
+                String aBuffer = "";
+                //do ip napocitam kolko riadkov mam
+                int ip=0;
+
+                while ((aDataRow = myReader.readLine()) != null) {
+                    aBuffer += aDataRow + "\n";
+                    ip = ip+1;
+
+                }
+
+                datas = new String[ip];
+                asks = new String[ip];
+                bids = new String[ip];
+                profs = new String[ip];
+
+                String kosikx = aBuffer;
+                myReader.close();
+
+                String delims = "[\n]+";
+                String delims2 = "[;]+";
+
+                String[] riadokxxx = kosikx.split(delims);
+                ipx = ip + "";
+                ip=ip-10;
+
+                for (int i = 0; i < riadokxxx.length; i++) {
+                    String riadok1 =  riadokxxx[i];
+
+                    String[] polozkyx = riadok1.split(delims2);
+                    cplx =  polozkyx[0];
+                    String idx =  polozkyx[1];
+                    String namex =  polozkyx[2];
+                    String pricex =  polozkyx[3];
+                    String mnozx =  polozkyx[4];
+                    String eanx =  polozkyx[5];
+
+                    String mnozxx =  mnozx + " x";
+
+                    // creating new HashMap
+                    HashMap<String, String> map = new HashMap<String, String>();
+
+                    // adding each child node to HashMap key => value
+                    map.put(TAG_PID, idx);
+                    map.put(TAG_NAME, idx + " " + namex + " " + eanx + "/" + cplx);
+                    map.put(TAG_PRICE, pricex);
+                    map.put(TAG_MNOX, mnozxx);
+                    map.put(TAG_CPL, cplx);
+
+                    // adding HashList to ArrayList dam tam len poslednych 10 i >= ip
+                    if( i >= ip ) { productsList.add(map); }
+
+                    datas[i] = idx;
+                    asks[i] = idx;
+                    bids[i] = idx;
+                    profs[i] = idx;
+
+                }
+
+                mapComparator = new Comparator<Map<String, String>>() {
+                    public int compare(Map<String, String> m1, Map<String, String> m2) {
+                        return m1.get("cpl").compareTo(m2.get("cpl"));
+                    }
+                };
+
+                Collections.sort(productsList, mapComparator);
+                //Log.d("productsList", productsList.toString());
+
+
+            } catch (Exception e) {
+                //Toast.makeText(getBaseContext(), e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+
+            return null;
+
+        }
+
+
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+
+                    myAskList = new ArrayList<>(Arrays.asList(asks));
+                    myBidList = new ArrayList<>(Arrays.asList(bids));
+                    myProfitList = new ArrayList<>(Arrays.asList(profs));
+                    mDataSet = new ArrayList<>(Arrays.asList(datas));
+                    //Log.d("mDataSet", mDataSet.toString());
+                    //Log.d("myBidList", myBidList.toString());
+
+                    adapter = new InventuraSDnewAdapter(InventuraSDnewActivity.this, mDataSet, myAskList, myBidList, myProfitList);
+                    recyclerView.setAdapter(adapter);
+
+
+
+                    inputAll = (EditText) findViewById(R.id.inputAll);
+                    inputAll.setText(cplx);
+
+                    if (cplx != null && !cplx.isEmpty()) {
+                        // doSomething
+                    }else {
+                        inputAll.setText("0");
+                    }
+
+                    popisEan = (TextView) findViewById(R.id.popisEan);
+                    popisEan.setText(ipx + " x " + getResources().getString(R.string.popisean));
+
+                    //adapter.notifyDataSetChanged();
+
+                }
+            });
+
+        }
+
+    }
+    //end loadallproducts
+
+
+}//end of activity
---
+++ app/src/main/java/com/eusecom/saminveantory/InventuraSDnewAdapter.java
@@ -0,0 +1,186 @@
+package com.eusecom.saminveantory;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by Wasabeef on 2015/01/03.
+ */
+@SuppressWarnings("deprecation")
+public class InventuraSDnewAdapter extends RecyclerView.Adapter<InventuraSDnewAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<String> mDataSet;
+    private List<String> mAskPrice;
+    private List<String> mBidPrice;
+    private List<String> mProfit;
+
+    public InventuraSDnewAdapter(Context context, List<String> dataSet, List<String> askPrice,
+    		List<String> bidPrice, List<String> Profit) {
+    	
+        mContext = context;
+        mDataSet = dataSet;
+        mAskPrice = askPrice;
+        mBidPrice = bidPrice;
+        mProfit = Profit;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {   	
+        
+        View v = LayoutInflater.from(mContext)
+                .inflate(R.layout.layout_list_invsd, parent, false);
+        return new ViewHolder(v);
+        
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Picasso.with(mContext).load(R.drawable.add2new).into(holder.image);
+        holder.text.setText(mDataSet.get(position));
+        holder.askprice.setText(mAskPrice.get(position));
+        holder.bidprice.setText(mBidPrice.get(position));
+        holder.profit.setText(mProfit.get(position));
+        
+        holder.setClickListener(new InventuraSDnewAdapter.ViewHolder.ClickListener() {
+            public void onClick(View v, int pos, boolean isLongClick) {
+                if (isLongClick) {
+         
+                    // View v at position pos is long-clicked.
+                	String poslx = pos + "";
+                	String menax = mDataSet.get(pos);
+                	Toast.makeText(mContext, "longclick pos. " + poslx + " pair " + menax, Toast.LENGTH_SHORT).show();
+                	
+                } else {
+                    // View v at position pos is clicked.
+                	//String possx = pos + "";
+                	String poslx = pos + "";
+                	String mena2 = mDataSet.get(pos);
+                	Toast.makeText(mContext, "shortclick pos. " + poslx + " pair " + mena2, Toast.LENGTH_SHORT).show();
+                	//toggleSelection(pos);
+                	
+                	Intent i = new Intent(mContext, InventuraSDnewActivity.class);
+                	Bundle extras = new Bundle();
+                    extras.putString("pairx", mena2);
+                    extras.putInt("whatspage", 0);
+                    i.putExtras(extras);
+                    v.getContext().startActivity(i);
+                    //use localbroadcast or interface for finish activity and asynctask
+                    //mActivity.finish(); don't work if i send activity to adapter
+                    
+                    sendValueToFavAct("A", 1);
+
+                    
+                }
+            }
+        });
+        
+    }
+    
+    //sending values from fragment to activity
+  	protected void sendValueToFavAct(String value, int xxsp) {
+          // it has to be the same name as in the fragment
+          Intent intent = new Intent("com.eusecom.exforu.action.UI_FINISH_FAVACT");
+          Bundle dataBundle = new Bundle();
+          dataBundle.putInt("UI_XXSP", xxsp);
+          dataBundle.putString("UI_VALUE", value);
+          intent.putExtras(dataBundle);
+          
+          Log.d("FavoriteAdapter", "I am at sendValueToFavAct.");
+          LocalBroadcastManager.getInstance(mContext).sendBroadcast(intent);
+          
+        
+      }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
+    public void remove(int position) {
+        mDataSet.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void add(String text, int position) {
+        mDataSet.add(position, text);
+        notifyItemInserted(position);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+
+        public ImageView image;
+        public TextView text;
+        public TextView askprice;
+        public TextView bidprice;
+        public TextView profit;
+        private ClickListener clickListener;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = (ImageView) itemView.findViewById(R.id.image);
+            text = (TextView) itemView.findViewById(R.id.text);
+            askprice = (TextView) itemView.findViewById(R.id.askprice);
+            bidprice = (TextView) itemView.findViewById(R.id.bidprice);
+            profit = (TextView) itemView.findViewById(R.id.profit);
+            
+            // We set listeners to the whole item view, but we could also
+            // specify listeners for the title or the icon.
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+            
+        }
+        
+        
+        /* Interface for handling clicks - both normal and long ones. */
+        public interface ClickListener {
+
+            /**
+             * Called when the view is clicked.
+             *
+             * @param v view that is clicked
+             * @param position of the clicked item
+             * @param isLongClick true if long click, false otherwise
+             */
+            public void onClick(View v, int position, boolean isLongClick);
+
+        }
+
+        /* Setter for listener. */
+        public void setClickListener(ClickListener clickListener) {
+            this.clickListener = clickListener;
+        }
+
+    	@Override
+        public void onClick(View v) {
+
+            // If not long clicked, pass last variable as false.
+            clickListener.onClick(v, getPosition(), false);
+        }
+
+    	@Override
+        public boolean onLongClick(View v) {
+
+            // If long clicked, passed last variable as true.
+            clickListener.onClick(v, getPosition(), true);
+            return true;
+        }
+
+  
+    }
+}
